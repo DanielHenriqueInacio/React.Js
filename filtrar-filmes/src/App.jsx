68cf8bdcd2afe087869aa745c320b35099ba83b1
Daniel Inacio <danielhi2013@hotmail.com>
2024-03-12 18:51:08 -0300

    Começo do projeto de listar filmes, apagar, deletar e editar

---
+++ filtrar-filmes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../public/css/App.css'
+import Table from './Components/Table'
+
+const App = () => {
+  return (
+    <div>
+      <Table />
+    </div>
+  )
+}
+
+export default App
+
