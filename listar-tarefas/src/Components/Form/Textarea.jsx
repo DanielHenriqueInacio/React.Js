7d5a0720d98573fff72ea86d623d122f32e51c46
Daniel Inacio <danielhi2013@hotmail.com>
2024-05-15 15:13:23 -0300

    Ajustando projeto para tirar pequenos erros

--- listar-tarefas/src/Components/Form/Textarea.jsx
+++ listar-tarefas/src/Components/Form/Textarea.jsx
@@ -4,7 +4,7 @@ const Textarea = ({ label, name, value, onChange }) => {
   return (
     <div className='mt-5'>
       <label htmlFor={name} className='block text-xl font-josefin text-center text-gray-700 mb-1'>{label}</label>
-      <textarea name={name} id={name} rows="3" defaultValue={value} onChange={onChange} className='w-80 border border-slate-400 rounded-md py-2 px-2 focus:outline-none focus:bg-gray-100'></textarea>
+      <textarea name={name} id={name} rows="3" defaultValue={value} onChange={onChange} className='w-80 border border-slate-400 rounded-md py-2 px-2 focus:outline-none focus:bg-gray-100 resize-none'></textarea>
     </div>
   )
 }
